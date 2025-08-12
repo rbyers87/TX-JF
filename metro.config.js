f2de91dec01813446a43a871ec8a0c774468b820
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-08-12 17:22:55 -0500

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add support for additional asset extensions if needed
-config.resolver.assetExts.push('bin');
-
-module.exports = config;
